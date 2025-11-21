a89bc0cd6894f72752a303b70cbf57a34e95ec90
Othallia <vianiputriocta6@gmail.com>
2025-11-21 17:35:55 +0700

    menambahkan fungsi TampilData dan mapping data Siswa

---
+++ app/src/main/java/com/example/quest6_032/view/TampilSiswa.kt
@@ -0,0 +1,40 @@
+package com.example.quest6_032.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quest6_032.R
+import com.example.quest6_032.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    siswaUiState: Siswa,
+    onBackBtnClick: () -> Unit,
+) {
+    val items = listOf(
+        Pair("Nama", siswaUiState.nama),
+        Pair("Gender", siswaUiState.gender),
+        Pair("Alamat", siswaUiState.alamat)
+    )
+
